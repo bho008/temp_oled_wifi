cae5ece3c1ad9baab07cdb5528c20632d98c5e62
bho008 <bho008@ucr.edu>
2015-11-28 17:49:39 -0800

    first working before changing to header files

---
+++ temp_oled_wifi/oled_setup.h
@@ -0,0 +1,30 @@
+﻿#ifndef oled_setup_h
+#define oled_sedup_h
+//#include "I2C.h"
+#include "Arduino.h"
+
+#define OLED_address  0x3c
+#define INTERNAL_I2C_PULLUPS
+#include <avr/pgmspace.h>
+#include <avr/io.h>
+
+class OLED_I2C {
+	private:
+		
+	public: 
+
+		void print_a_char(unsigned char);
+		void init();
+		void sendcommand(unsigned char);
+		void clear_display(void);
+		void SendChar(unsigned char);
+		void setXY(unsigned char row,unsigned char col);
+		void sendStr(unsigned char *string);
+		void init_OLED(void);
+		void i2c_OLED_init(void);
+	
+};
+
+extern OLED_I2C oled;
+
+#endif
